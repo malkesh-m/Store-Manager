61a543cec76d36727b1c7663335e1f6bdebaf29d
malkesh-m <malkesh-m@gmail.com>
2020-12-25 11:26:20 +0530

    Report & Profile Settings

---
+++ src/configs/constant.js
@@ -0,0 +1,6 @@
+export const API = 'https://food.rnlab.io';
+
+export const CONSUMER_KEY = 'ck_f3b113414fc6f6b58df59791c231bb390364e3e6';
+export const CONSUMER_SECRET = 'cs_1693429998e6d95c769c0e46a75627bf2f2c0325';
+
+export const ONE_SIGNAL_APP_ID = '44c06d97-ecd6-4589-9478-c62dbbbe0d4c';
